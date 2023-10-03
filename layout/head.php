1b295079dacfe694b4a0de2b2d59e815c1224838
szabomarcel <szabomarcell34@gmail.com>
2023-10-03 15:27:31 +0200

    main

--- layout/head.php
+++ layout/head.php
@@ -3,5 +3,5 @@
     <head>
         <meta charset="UTF-8">
         <title>Állat menhely</title>
-        <link rel="stylesheet" href="./bootstrap-5.2.3-dist/css/bootstrap.min.css"
+        <link rel="stylesheet" href="./bootstrap-5.2.3-dist/css/bootstrap.min.css">
     </head>
